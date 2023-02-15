5800b1c65a272f13d9a3885249a6b315fdf744b2
Obiaderi <edoborobiaderi@gmail.com>
2023-02-15 15:17:44 -0800

    Client side updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.set("strictQuery", false);
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDB;
